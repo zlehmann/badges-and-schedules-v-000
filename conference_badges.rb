a4a335479a56b3afa53724e4cef25b4527ba67bd
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-09 15:05:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my nmae is #{name}"
+end
+
+def batch_badge_creator(array)
+  badge_array = []
+  array.each { |element| badge_array.push(badge_maker(element)) }
+  return badge_array
+end
