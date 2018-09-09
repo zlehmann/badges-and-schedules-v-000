00c60811af8181268ae6ad4381087598b01174c0
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-09 15:18:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,11 @@ def batch_badge_creator(array)
   array.each { |element| badge_array.push(badge_maker(element)) }
   return badge_array
 end
+
+def assign_rooms(speakers)
+  room_assign = []
+  speakers.each_with_index do |speaker, index|
+    room_assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+  end
+  return room_assign
+end
